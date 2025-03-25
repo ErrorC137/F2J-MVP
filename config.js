4037154017dc68cb3dce856977e7617ff91a6cd7
ErrorC137 <154239085+ErrorC137@users.noreply.github.com>
2025-03-25 23:01:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 const XRPL_TESTNET_WS = "wss://s.altnet.rippletest.net:51233";
+
 const F2J_TOKEN = {
     currency: "F2J",
-    issuer: "rPT1Sjq2YGrBMTttX4GZHjKu9dyfzbpAYe",
+    issuer: process.env.ISSUER_ADDRESS || "rPT1Sjq2YGrBMTttX4GZHjKu9dyfzbpAYe",
     value: "0"
 };
-const PLATFORM_WALLET = "rPT1Sjq2YGrBMTttX4GZHjKu9dyfzbpAYe";
+
+const PLATFORM_WALLET = process.env.PLATFORM_WALLET || "rPT1Sjq2YGrBMTttX4GZHjKu9dyfzbpAYe";
+
